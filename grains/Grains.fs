5a31f2a92b08e777fcc3e081cc41cd862e9f1caa
hacaonguyen <hacaonguyen@yahoo.com>
2019-10-18 10:30:01 +0700

    add graind
    add graind

--- grains/Grains.fs
+++ grains/Grains.fs
@@ -1,4 +1,12 @@
 ﻿module Grains
 
-let square (n: int): Result<uint64,string> = failwith "You need to implement this function."
-let total: Result<uint64,string> = failwith "You need to implement this function."
+open System
+
+let grain = seq<uint64> {for i = 0 to 63 do yield uint64(2.0**float(i))}
+
+let square (n: int): Result<uint64,string> = 
+    if n < 1 || n > 64 then Error "square must be between 1 and 64" 
+    else grain |> (Seq.item (n-1)) |> Ok 
+
+let total: Result<uint64,string> =
+    grain |> Seq.sum |> Ok
