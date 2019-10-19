1bce005ca8b26ab55c255b7b113686bb86c9524b
hacaonguyen <hacaonguyen@yahoo.com>
2019-10-19 15:26:57 +0700

    Update TreeEval2.fs

--- NguyenBX/TreeEval2.fs
+++ NguyenBX/TreeEval2.fs
@@ -12,26 +12,11 @@ type BoolExpTree =
     | AndBranch of BoolExpTree list
     | NotBranch of BoolExpTree
 
-type BranchType =
-    | AndTree
-    | OrTree
-
 let rec evalBoolExpTree (tree: BoolExpTree): bool =
-    let rec evalTreeList (trees: BoolExpTree list) (treeType: BranchType): bool =
-        match trees with
-        | [] -> true
-        | head::tail ->
-            let x = evalBoolExpTree head
-            match (treeType, x) with
-            | AndTree, false -> false
-            | AndTree, _ -> x && (evalTreeList tail AndTree)
-            | OrTree, true -> true
-            | OrTree, _ -> x || (evalTreeList tail OrTree)
-            //match treeType with
-            //| AndTree -> x && (evalTreeList tail AndTree)
-            //| OrTree -> x || (evalTreeList tail OrTree) 
     match tree with
-    | Leaf x -> x
-    | NotBranch tree -> not (evalBoolExpTree tree)
-    | AndBranch trees -> evalTreeList trees AndTree
-    | OrBranch trees -> evalTreeList trees OrTree
+    | Leaf      x       -> x
+    | NotBranch tree    -> not (evalBoolExpTree tree)
+    | AndBranch []      -> true
+    | AndBranch trees   -> trees |> List.fold (fun a i -> a && (evalBoolExpTree i)) true
+    | OrBranch  []      -> true
+    | OrBranch  trees   -> trees |> List.fold (fun a i -> a || (evalBoolExpTree i)) false
