f09dfb7983b65fe9b7cacf9d8472dd50be0e071d
hacaonguyen <hacaonguyen@yahoo.com>
2019-10-20 11:32:33 +0700

    Update Randrop
    Update Randrop using list choose

--- raindrops/Raindrops.fs
+++ raindrops/Raindrops.fs
@@ -3,10 +3,11 @@ open System
 
 let convert (number: int): string = 
     [(3, "Pling"); (5, "Plang"); (7, "Plong")] 
-    |> List.map (fun(n, s) -> if number % n = 0 then s else "") 
-    |> String.Concat
+    |> List.choose (fun(n, s) -> if number % n = 0 then Some s else None) 
     |> function
-        | "" -> number.ToString()
-        | x -> x        
+        | [] -> number.ToString()
+        | x -> x |> String.Concat
 
+//can also use String.concat "" xs 
+//with xs is a string list
 
