64d1a69c3f2f25fef5e4b6e72ec049f3d23633fe
hacaonguyen <hacaonguyen@yahoo.com>
2019-11-03 07:25:14 +0700

    Some change
    None

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -5,19 +5,18 @@ type School = Map<int, string list>
 let empty: School = Map.empty
 
 let add (student: string) (grade: int) (school: School): School = 
-    match (Map.tryFind grade school) with //get current lists
-    | None -> school |> Map.add grade [student] 
-    | Some students -> school |> Map.add grade (student :: students) 
-
+    let students = school |> Map.tryFind grade |> Option.defaultValue [] //get current lists
+    school |> Map.add grade (student :: students)
+ 
 let roster (school: School): string list = 
     school 
     |> Map.toList 
-    |> List.sortBy (fun (i, s) -> i) //sort by grade
-    |> List.map (fun (i, s) -> s |> List.sort) //get name and sort by name
+    |> List.sortBy fst //sort by grade
+    |> List.map (snd >> List.sort) //get name and sort by name
     |> List.concat 
 
 let grade (number: int) (school: School): string list = 
-    match (Map.tryFind number school) with
-    | None -> [] 
-    | Some students -> students |> List.sort 
-    
+    school 
+    |> Map.tryFind number
+    |> Option.defaultValue []
+    |> List.sort
