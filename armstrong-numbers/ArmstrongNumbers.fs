f4356e56c1358fb7a5ccd78a4cfe18c1199b5db4
hacaonguyen <hacaonguyen@yahoo.com>
2019-11-26 22:14:40 +0700

    ArmstrongNumbers
    None

---
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,18 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber (number: int): bool = 
+    let str = number |> string
+    let len = str |> String.length
+    let number' = 
+        str
+        |> Seq.map (fun c -> int c - int '0')
+        |> Seq.sumBy (fun i -> (pown i len))
+    number = number'
+
+(*
+other ways to convert char -> int
+c |> string |> int
+c |> (string >> int)
+
+we can use: Seq.map (string >> int) 
+*)
