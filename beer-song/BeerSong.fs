ed94c7007f02f6b5e1cdcf30ac4932f90e9abf9b
hacaonguyen <hacaonguyen@yahoo.com>
2019-12-03 11:13:54 +0700

    Beer
    Beer

---
+++ beer-song/BeerSong.fs
@@ -0,0 +1,21 @@
+﻿module BeerSong
+
+//recite item n
+let item n =     
+    match n with
+    | 0 -> [
+        "No more bottles of beer on the wall, no more bottles of beer.";
+        "Go to the store and buy some more, 99 bottles of beer on the wall." ] 
+    | 1 -> [
+        "1 bottle of beer on the wall, 1 bottle of beer.";
+        "Take it down and pass it around, no more bottles of beer on the wall." ] 
+    
+    | _ -> [
+        yield sprintf "%i bottles of beer on the wall, %i bottles of beer." n n
+        let s = if n = 2 then "1 bottle" else sprintf "%i bottles" (n - 1) 
+        yield sprintf "Take one down and pass it around, %s of beer on the wall." s ]
+
+let recite (startBottles: int) (takeDown: int) =  
+    [1..takeDown]
+    |> List.collect (fun i -> item (startBottles - i + 1) |> List.append [""])
+    |> List.tail
---
+++ rational-numbers/RationalNumbers.fs
@@ -0,0 +1,40 @@
+﻿module RationalNumbers
+
+//greatest common divisor
+let rec gcd a b = 
+    match a, b with
+    | 0, _ -> b
+    | _, 0 -> a
+    | _ when a > b -> gcd b (a % b)
+    | _ -> gcd a (b % a)
+
+let reduce (a, b) = 
+    let x = gcd (abs a) (abs b)
+    if b < 0 then (-a / x, -b / x) else (a / x, b / x)
+
+let create a b = (a, b)
+
+let add (a1, b1) (a2, b2) = (a1*b2 + a2*b1, b1*b2) |> reduce
+
+let sub (a1, b1) (a2, b2) = (a1*b2 - a2*b1, b1*b2) |> reduce 
+
+let mul (a1, b1) (a2, b2) = (a1*a2, b1*b2) |> reduce
+
+let div (a1, b1) (a2, b2) = (a1*b2, b1*a2) |> reduce
+
+let abs (a, b) = (abs a, abs b) |> reduce
+
+let exprational n (a, b) = (pown a n, pown b n) |> reduce
+
+let expreal (a, b) n = (float n) ** ((float a) / (float b))
+
+(*
+//another way to implement gcd function
+let rec gcd2 (a: int) (b: int): int =
+    match a, b with
+    | 0, _ -> b
+    | _, 0 -> a 
+    | _ when a = b -> a
+    | _ when a > b -> gcd2 b (a - b)
+    | _ -> gcd2 a (b - a)
+*)
