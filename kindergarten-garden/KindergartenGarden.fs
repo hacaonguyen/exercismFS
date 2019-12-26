c92b46f22f561fb8cddd6f759399ce5218951b10
hacaonguyen <hacaonguyen@yahoo.com>
2019-12-26 08:25:20 +0700

    OK
    OK

---
+++ kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,22 @@
+﻿module KindergartenGarden
+
+type Plant = Clover | Grass | Radishes | Violets 
+
+let toPlant char = 
+    match char with
+    | 'C' -> Plant.Clover
+    | 'G' -> Plant.Grass
+    | 'R' -> Plant.Radishes
+    | _   -> Plant.Violets
+
+let plants (diagram: string) (student: string) = 
+    let diagrams = diagram.Split '\n'
+    let position = (int (student.ToUpper().[0]) - int 'A') * 2
+    
+    [ 
+        diagrams.[0].[position]; 
+        diagrams.[0].[position + 1]; 
+        diagrams.[1].[position]; 
+        diagrams.[1].[position + 1]
+    ] 
+    |> List.map toPlant
---
+++ robot-simulator/RobotSimulator.fs
@@ -0,0 +1,49 @@
+﻿module RobotSimulator
+
+type Direction = North | East | South | West
+type Position = int * int
+type Robot = { direction: Direction; position: Position }
+
+let create direction position = 
+    {Robot.direction = direction; Robot.position = position}
+
+let moveR (robot: Robot): Robot =
+    let direction = 
+        match robot.direction with
+        | North -> East
+        | East  -> South
+        | South -> West
+        | West  -> North
+    
+    { robot with direction = direction } 
+    
+let moveL (robot: Robot): Robot =
+    let direction = 
+        match robot.direction with
+        | North -> West
+        | East  -> North
+        | South -> East
+        | West  -> South
+    
+    { robot with direction = direction } 
+
+let moveA (robot: Robot): Robot =
+    let (x, y) = robot.position
+    let position = 
+        match robot.direction with
+        | North -> (x, y + 1)
+        | East  -> (x + 1, y)
+        | South -> (x, y - 1)
+        | West  -> (x - 1, y)
+
+    { robot with position = position } 
+
+let move1Step robot instruction = 
+    match instruction with
+    | 'L' -> moveL robot
+    | 'R' -> moveR robot
+    | 'A' -> moveA robot
+    | _   -> failwith "Invalid instruction!"
+
+let move instructions robot = 
+    Seq.fold move1Step robot instructions
