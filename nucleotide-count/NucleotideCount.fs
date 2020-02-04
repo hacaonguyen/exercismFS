f1db8fe19d767efa6c1c40a6f1f6ee9e3dc2fce4
hacaonguyen <hacaonguyen@yahoo.com>
2020-02-04 11:34:35 +0700

    add Nucleotide
    add Nucleotide

---
+++ nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,20 @@
+﻿module NucleotideCount
+
+let count char string =
+    string |> Seq.filter ((=) char) |> Seq.length
+
+let validStrand (strand: string) =
+    let validSet = "ACGT" |> Set.ofSeq
+    strand |> Set.ofSeq |> Set.isSuperset validSet
+
+let nucleotideCounts (strand: string): Option<Map<char, int>> =  
+    if validStrand strand then
+        "ACGT"
+        |> Seq.map (fun c -> (c, count c strand))
+        |> Map.ofSeq
+        |> Some
+    else
+        None
+
+
+
