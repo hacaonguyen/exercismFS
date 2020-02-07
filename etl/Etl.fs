fdbd07be42f650e66cfc1063413d0e8b80f4d1f0
hacaonguyen <hacaonguyen@yahoo.com>
2020-02-07 08:14:03 +0700

    add ETL
    add ETL

---
+++ etl/Etl.fs
@@ -0,0 +1,11 @@
+﻿module Etl
+
+let transformItem (number, charList) =  
+    charList |> List.fold (fun acc char -> (char, number)::acc) []
+
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> = 
+    scoresWithLetters
+    |> Map.toList
+    |> List.fold (fun acc item -> acc @ (transformItem item)) []
+    |> List.map (fun (c, n) -> (System.Char.ToLower c ,n))
+    |> Map.ofList
